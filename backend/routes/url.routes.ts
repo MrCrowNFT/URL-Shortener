92bb67ba51af00230ef063c0063ba8d3a73e6758
MrCrowNFT <mrcrownft@gmail.com>
2025-02-04 11:07:09 -0300

    get URL route added

--- backend/routes/url.routes.ts
+++ backend/routes/url.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPair } from "../controllers/url.controller";
+import { createPair, getOriginalUrl } from "../controllers/url.controller";
 
 const urlRouter = express.Router();
 
 
 urlRouter.post("/new", createPair);
+urlRouter.get("/:shortUrl", getOriginalUrl);
 
 export default urlRouter;
