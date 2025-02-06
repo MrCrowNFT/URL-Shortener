7aed6b253185b541e04b16e7755c154c213f8e2e
MrCrowNFT <mrcrownft@gmail.com>
2025-02-06 19:41:04 -0300

    Home page layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,20 @@
 
 
 const HomePage = (()=>{
-    return <></>
+    return (
+    <div>
+        <div>
+            <img></img>
+            <label>Paste URL:</label>
+            <input type="url" />
+            <div>
+                <img></img>
+                <img></img>
+                <img></img>
+            </div>
+        </div>
+    </div>
+    )
 })
 
 export default HomePage
