e7e4e959f46084bcdbe97913cffbd5ab07465ae4
MrCrowNFT <mrcrownft@gmail.com>
2025-02-07 07:41:43 -0300

    Home page css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 
 
 import './App.css'
+import HomePage from './pages/Home'
 
 function App() {
 
   return (
     <>
-
-       
+      <HomePage/>
     </>
   )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,21 +2,38 @@ import facebook from "../assets/facebook.svg";
 import instagram from "../assets/instagram.svg";
 import twitter from "../assets/twitter.svg";
 
-const HomePage = (()=>{
-    return (
-    <div>
-        <div>
-            <img></img>
-            <label>Paste URL:</label>
-            <input type="url" />
-            <div>
-                <img src={facebook}/>
-                <img src={instagram}/>
-                <img src={twitter}/>
-            </div>
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col border items-center justify-center border-gray-400 rounded-lg w-140 h-220">
+        <img />
+        <div className="flex items-center justify-center gap-0.5">
+          <label>Paste URL:  </label>
+          <form>
+            <input
+              type="url"
+              placeholder="Insert URL"
+              autoComplete="off"
+              autoCapitalize="off"
+              className="bg-transparent focus:outline-none text-2xl p-2"
+            />
+          </form>
         </div>
+        <hr />
+        <div className="flex items-center gap-2">
+          <a href="" target="_blank" rel="noreferrer">
+            <img src={facebook} alt="facebook" className="h-10 w-10" />
+          </a>
+          <a href="" target="_blank" rel="noreferrer">
+            <img src={instagram} alt="instagram" className="h-10 w-10" />
+          </a>
+          <a href="" target="_blank" rel="noreferrer">
+            <img src={twitter} alt="twitter" className="h-10 w-10" />
+          </a>
+        </div>
+      </div>
     </div>
-    )
-})
+  );
+};
 
-export default HomePage
+export default HomePage;
