492bcdff5ddb4377e9418acf360dd0c732321ed0
MrCrowNFT <mrcrownft@gmail.com>
2025-02-08 20:25:26 -0300

    Front end tweaks

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,15 +5,15 @@ import logo from "../assets/logo.svg";
 
 const HomePage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black/90">
-      <div className="flex flex-col border items-center justify-items-start border-gray-400 rounded-lg w-130 h-220  shadow-lg">
-        <img src={logo} alt="logo" className="h-100" />
-        <h1 className="text-5xl font-mono font-bold mb-6">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-black to-gray-900">
+      <div className="flex flex-col items-center border border-gray-500 rounded-2xl w-130 h-220 shadow-2xl bg-gray-800 p-8">
+        <img src={logo} alt="logo" className="h-75" />
+        <h1 className="text-6xl font-mono font-bold mb-8">
           <span className="text-indigo-700">Url</span>{" "}
           <span className="text-green-500">Shortener</span>
         </h1>
 
-        <form className="border border-gray-400 rounded-lg h-15 w-110 flex items-center justify-center mt-5">
+        <form className="border border-gray-500 rounded-lg h-16 w-110 flex items-center shadow-md">
           
           <input
             type="url"
@@ -23,7 +23,7 @@ const HomePage = () => {
             className="text-white flex-1 placeholder-gray-400 placeholder-font-bold placeholder-font-mono bg-transparent focus:outline-none text-2xl p-2 w-full"
           />
 
-          <button className=" bg-gradient-to-br from-indigo-700 to-green-500 font-mono font-bold text-white py-4.5 px-4 rounded-r-lg transition-all h-full hover:opacity-85">
+          <button className=" bg-gradient-to-br from-indigo-700 to-green-500 font-mono font-bold text-white py-4.5 px-4 rounded-r-lg transition-all h-full hover:opacity-85 hover:scale-105">
             Shorten
           </button>
         </form>
@@ -31,18 +31,18 @@ const HomePage = () => {
         <hr className=" w-full my-8 border-t border-gray-400 mt-35" />
 
         <div className="flex flex-col items-center justify-center w-full h-32">
-          <div className="flex items-center justify-center gap-3">
-            <a href="" target="_blank" rel="noreferrer">
-              <img src={facebook} alt="facebook" className="h-10 w-10 hover:scale-110 transition-transform duration-300" />
+          <div className="flex items-center justify-center gap-4">
+            <a href="" target="_blank" rel="noreferrer" className="hover:scale-110 transition-transform duration-300">
+              <img src={facebook} alt="facebook" className="h-12 w-12 " />
             </a>
-            <a href="" target="_blank" rel="noreferrer">
-              <img src={instagram} alt="instagram" className="h-10 w-10 hover:scale-110 transition-transform duration-300" />
+            <a href="" target="_blank" rel="noreferrer" className="hover:scale-110 transition-transform duration-300">
+              <img src={instagram} alt="instagram" className="h-12 w-12 " />
             </a>
-            <a href="" target="_blank" rel="noreferrer">
-              <img src={twitter} alt="twitter" className="h-10 w-10 hover:scale-110 transition-transform duration-300" />
+            <a href="" target="_blank" rel="noreferrer" className="hover:scale-110 transition-transform duration-300">
+              <img src={twitter} alt="twitter" className="h-12 w-12" />
             </a>
           </div>
-          <p className="font-mono font-bold text-white mt-3">Follow us!</p>
+          <p className="font-mono font-bold text-white text-lg mt-3">Follow us!</p>
         </div>
       </div>
     </div>
