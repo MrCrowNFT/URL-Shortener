f163d6332d58d8d6b418bd89df1e2010bcffa5d3
MrCrowNFT <mrcrownft@gmail.com>
2025-02-10 12:27:04 -0300

    testing env

--- backend/app.ts
+++ backend/app.ts
@@ -8,13 +8,3 @@ app.use("/link", urlRouter);
 
 export default app
 
-
-import dotenv from "dotenv";
-
-dotenv.config();
-const PORT = process.env.PORT || 5500;
-
-
-app.listen(PORT, () => {
-  console.log("Server started at http://localhost:" + PORT);
-});
---
+++ tests/url.test.ts
@@ -0,0 +1,8 @@
+import request from "supertest";
+import app from "../backend/app";
+
+describe("URL Shortener API", () => {
+  it("should create a short URL", async () => {
+    
+  });
+});
